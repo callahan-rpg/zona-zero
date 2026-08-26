503e31d9f7e52a82664defb49cc124d721252c7c
callahan-rpg <sterlingdotevander@gmail.com>
2026-08-25 23:21:57 -0300

    Update Location.jsx

--- src/pages/Location.jsx
+++ src/pages/Location.jsx
@@ -193,7 +193,6 @@ export default function Location() {
 
           {/* Chat central */}
           <div className="chat-container">
-            <div className="chat-location-label">{location.name}</div>
 
             <div className="chat-wrapper">
               <iframe
